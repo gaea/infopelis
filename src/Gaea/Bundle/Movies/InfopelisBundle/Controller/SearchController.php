3062ad071fef5add5efa0c36eb9fd53e816415f1
gaea <gaea.uv@gmail.com>
2014-11-23 15:09:12 -0500

    controller documentation

--- src/Gaea/Bundle/Movies/InfopelisBundle/Controller/SearchController.php
+++ src/Gaea/Bundle/Movies/InfopelisBundle/Controller/SearchController.php
@@ -1,19 +1,36 @@
 <?php
-
+/**
+ * This file is part of the infopelis aplication created by Gustavo Espinosa.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Gaea
+ * @author Gustavo Espinosa <gaea.uv@gmail.com>
+ * @version 0.0.1
+ */
 namespace Gaea\Bundle\Movies\InfopelisBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
 use Tmdb\Client;
-use Tmdb\Model\Search\SearchQuery\PersonSearchQuery;
-use Tmdb\Repository\SearchRepository;
-use Tmdb\Repository\PeopleRepository;
 
+/**
+ * Class SearchController
+ * @package Gaea\Bundle\Movies\InfopelisBundle\Controller
+ */
 class SearchController extends Controller
 {  
+	/**
+     * Get the discovery movies or movies (search for movies by title) information and show a list results
+     *
+     * @param Request $request
+     * @return mixed
+     */
 	public function indexAction(Request $request)
     {
+     * @param  array $parameters
+     * @param  array $headers
 		$movieTitle = $request->get('movie');
 		$client = new Client($this->get('TmdbToken'));
 		$movies = array();
@@ -44,7 +61,13 @@ class SearchController extends Controller
 		
         return $this->render('GaeaMoviesInfopelisBundle:Default:index.html.twig', $movies);
     }
-	 
+	
+	/**
+     * Search for people by name and show a list results.
+     *
+     * @param Request $request
+     * @return mixed
+     */
     public function searchAction(Request $request)
     {
 		$actor = $request->get('actor');
@@ -59,7 +82,14 @@ class SearchController extends Controller
         return $this->render('GaeaMoviesInfopelisBundle:Default:search.html.twig', $result);
     }
     
-    public function actorDetailAction(Request $request, $id, $order)
+     /**
+     * Get the general actor information and movie credits for a specific id.
+     *
+     * @param Request $request
+     * @param $id
+     * @return mixed
+     */
+    public function actorDetailAction(Request $request, $id)
     {
 		$actorId = $id;
 		$actorInfo = array();
@@ -87,6 +117,13 @@ class SearchController extends Controller
         return $this->render('GaeaMoviesInfopelisBundle:Default:actorDetail.html.twig', $actorInfo);
     }
     
+    /**
+     * Get the general movie information, cast and crew information for a specific id.
+     *
+     * @param Request $request
+     * @param $id
+     * @return mixed
+     */
     public function movieDetailAction(Request $request, $id)
     {
 		$movieId = $id;
